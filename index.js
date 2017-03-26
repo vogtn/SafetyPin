8052f86b144d00572084881c2f2e72cf710c4477
vogtn <nicolasjvogt@gmail.com>
2017-03-25 17:39:56 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./server/static/'));
+app.use(express.static('./client/dist/'));
+
+app.listen(3000, () => {
+    console.log('Server is running on localhost:3000')
+});
