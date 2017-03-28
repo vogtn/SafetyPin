3d8076aec225e161ef87580d8b4b7ebc234b88af
vogtn <nicolasjvogt@gmail.com>
2017-03-28 09:59:32 -0700

    adding auth

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/dist/'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+//routes
+const authRoutes = require('./server/routes/auth');
+app.use('/auth', authRoutes);
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on localhost:3000');
