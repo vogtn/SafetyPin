7226d8175b43f7376a7458c8d4bbcab1fbe0f5de
vogtn <nicolasjvogt@gmail.com>
2017-03-28 11:26:09 -0700

    preparing db models

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./config'); 
+
+//db
+require('./server/models').connect(config.dbUri);
 
 const app = express();
 
@@ -8,9 +13,22 @@ app.use(express.static('./client/dist/'));
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(passport.initialize());
+
+const localSignupStrategy = require('./server/passport/local-signup');
+const localLoginStrategy = require('./server/passport/local-login');
+
+passport.use('local-signup', localSignupStrategy);
+passport.use('local-login', localLoginStrategy);
+
+const authCheckMiddleware = require('./server/middleware/auth-check');
+app.use('/api', authCheckMiddleware);
+
 //routes
 const authRoutes = require('./server/routes/auth');
+const apiRoutes = require('./server/routes/api');
 app.use('/auth', authRoutes);
+app.use('/api', apiRoutes);
 
 
 
