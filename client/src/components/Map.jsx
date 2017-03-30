f7b4d836703cf1d419e96f536255abcf73fa1a0d
m-woo <mark.chi.woo@gmail.com>
2017-03-30 10:31:47 -0700

    map component and container

---
+++ client/src/components/Map.jsx
@@ -0,0 +1,9 @@
+import React, {PropTypes} from 'react';
+import {Link, IndexLink} from 'react-router';
+import MapResults from '../containers/MapResults'
+import Auth from '../modules/Auth';
+
+
+
+
+export default Map;
---
+++ client/src/containers/MapResults.jsx
@@ -0,0 +1,8 @@
+import React, {PropTypes} from 'react';
+const Link = require('react-router').Link;
+
+
+
+
+
+export default MapResults; 
